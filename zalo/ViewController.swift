040cce8ce7e114645a807649711348f9a6768ca6
duc <ducnnd00568@fpt.edu.vn>
2020-05-22 07:48:24 +0700

    Initial Commit

---
+++ zalo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  zalo
+//
+//  Created by Ngoduc on 5/22/20.
+//  Copyright © 2020 Ngoduc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
